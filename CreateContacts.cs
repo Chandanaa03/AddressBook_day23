f6341e62843b9eadc31a6591aed0309ef4c9e32f
chandana Reddy <chandanareshma01@gmail.com>
2022-03-31 23:25:42 +0530

    Creating details

---
+++ CreateContacts.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public class CreateContacts
+    {
+        
+
+
+
+            string FirstName { get; set; }
+            string LastName { get; set; }
+            string phonenumber { get; set; }
+            string city { get; set; }
+            string state { get; set; }
+            string Zip { get; set; }
+            string address { get; set; }
+
+            public List<CreateContacts> People = new List<CreateContacts>();
+            public void addperson()
+            {
+
+            CreateContacts person = new CreateContacts();
+
+
+                Console.WriteLine("Here you can add a entry to your address book");
+
+
+                Console.Write("Enter First Name: ");
+                person.FirstName = Console.ReadLine();
+
+                Console.Write("Enter Last Name: ");
+                person.LastName = Console.ReadLine();
+
+                Console.Write("Enter Phone Number: ");
+                person.phonenumber = Console.ReadLine();
+
+                Console.Write("Enter Address 1: ");
+
+                person.address = Console.ReadLine();
+                Console.Write("Enter Zip: ");
+                person.Zip = Console.ReadLine();
+
+                Console.Write("Enter city: ");
+                person.city = Console.ReadLine();
+
+                Console.Write("Enter state: ");
+                person.state = Console.ReadLine();
+
+
+                People.Add(person);
+            }
+        }
+
+
+
+    
+}
+
