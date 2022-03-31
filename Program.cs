511eae87658ddce02d61d32f5bc7434d9ba67440
chandana Reddy <chandanareshma01@gmail.com>
2022-03-31 23:22:09 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AddressBook
+{
+    /// <summary>
+    /// contains main method
+    /// </summary>
+    class program
+    {
+        /// <summary>
+        /// Displaying Greeting message
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book program");
+        }
+    }
+}
+
