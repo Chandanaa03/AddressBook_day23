6e6353e8702c89cd1d74314ca937aec0d76a26a1
chandana Reddy <chandanareshma01@gmail.com>
2022-04-01 00:23:55 +0530

    Delete Contact

---
+++ DeleteName.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public class DeleteName
+    {
+        public List<DeleteName> People = new List<DeleteName>();
+        public void deletecontact()
+        {
+            DeleteName person = new DeleteName();
+            Console.WriteLine("Enter the first name of the person you would like to remove.");
+            string FirstName = Console.ReadLine();
+
+            if (person == null)
+            {
+                Console.WriteLine("That person could not be found. Press any key to continue");
+                Console.ReadKey();
+                return;
+            }
+            else
+            {
+
+                People.Remove(person);
+                Console.WriteLine("Person removed. Press any key to continue.");
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
